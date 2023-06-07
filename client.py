5e54d94ee70cef79cbaf40d63e668e244e9db56e
StevenCigare <eryk.ulanowicz09@gmail.com>
2023-06-07 17:38:54 +0200

    Added CBC/ECB encrypting modes with radiobuttons

--- client.py
+++ client.py
@@ -2,26 +2,61 @@ import socket
 import tqdm
 import os
 import time
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import pad
 
 class Client():
     def __init__(self, address, port):
         self.send_address = address
         self.port = port
         self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.session_key = b''
+        self.cipher_mode = ''
     
     def send_text(self, text):
         self.client.send("send_text".encode())
-        self.client.send(text.encode())
+        print(f'MODE: {self.cipher_mode}')
+        self.client.send(self.cipher_mode.encode())
+        cipher = ''
+        text = bytes(text, 'utf-8')
+        if self.cipher_mode == 'CBC':
+            cipher = AES.new(self.session_key, AES.MODE_CBC)
+            ciphered_text = cipher.encrypt(pad(text, AES.block_size))
+
+            self.client.send(cipher.iv)
+            self.client.send(ciphered_text)
+
+        elif self.cipher_mode == 'ECB':
+            cipher = AES.new(self.session_key, AES.MODE_ECB)
+            ciphered_text = cipher.encrypt(pad(text, AES.block_size))
+
+            self.client.send(ciphered_text)
+        
+        else:
+            return
+        
+        
 
     def send_file(self, filepath):
         self.client.send("send_file".encode())
+        cipher = ''
+        print(f'MODE: {self.cipher_mode}')
+        self.client.send(self.cipher_mode.encode())
 
+        if self.cipher_mode == 'CBC':
+            cipher = AES.new(self.session_key, AES.MODE_CBC)
+            self.client.send(cipher.iv)
+        
+        elif self.cipher_mode == 'ECB':
+            cipher = AES.new(self.session_key, AES.MODE_ECB)
+
+        
+        
         with open(filepath, "rb") as f:
             file_size = os.path.getsize(filepath)
             idx = filepath[::-1].find('.')
             extension = filepath[-idx:]
-            self.client.send(f"received_file.{extension}".encode())
-            self.client.send(str(file_size).encode())
+            self.client.send(f"received_file.{extension}*{str(file_size)}".encode())
 
             data = f.read()
             progress = tqdm.tqdm(unit="8", unit_scale=True, unit_divisor=1000, total=int(file_size))
@@ -29,20 +64,28 @@ class Client():
 
             while True:
                 if packet_size > len(data):
-                    self.client.send(data)
+                    padded = pad(data, AES.block_size)
+                    encrypted = cipher.encrypt(padded)
+                    self.client.send(encrypted)
                     progress.update(1024)
                     break
    
-                time.sleep(0.08)
-                self.client.send(data[:packet_size])
+                time.sleep(0.15)
+                padded = pad(data[:packet_size], AES.block_size)
+
+                encrypted = cipher.encrypt(padded)
+                self.client.send(encrypted)
                 progress.update(1024)
                 data = data[packet_size:]
 
-            self.client.send(b"<END>")
+            self.client.send(cipher.encrypt(pad(b"<END>", AES.block_size)))
 
     def send_key(self, key):
-        self.client.send(key.encode())
+        self.client.send(key)
 
+
+    def set_cipher_mode(self, mode):
+        self.cipher_mode = mode
 
 
         
--- server.py
+++ server.py
@@ -1,40 +1,78 @@
 import socket
 import tqdm
-import os
 import time
-import asyncio
+from Crypto.Cipher import AES
+from Crypto.Util.Padding import unpad
+
 
 class Server():
     def __init__(self, address, port):
         self.recv_address = address
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server.bind((self.recv_address, port))
-        #self.key = self.client.recv(1024).decode()
+        self.session_key = b''
         
     def receive_file_from_client(self):
-        file_name = self.client.recv(1024).decode()
-        file_size = self.client.recv(1024).decode()
+        mode = self.client.recv(3).decode()
+        
+        print(f'MODE: {mode}')
+        cipher = ''
+        if mode == 'CBC':
+            iv = self.client.recv(16)
+            cipher = AES.new(self.session_key, AES.MODE_CBC, iv=iv)       
+        
+        elif mode == 'ECB':
+            cipher = AES.new(self.session_key, AES.MODE_ECB)
+
+        s = self.receive_text_from_client()
+        file_name, file_size = s.split('*')
         file = open(file_name, "wb")
         file_bytes = b""
         data = b""
         done = False
-        progress = tqdm.tqdm(unit="8", unit_scale=True, unit_divisor=1000, total=int(file_size))
+        
+        progress = tqdm.tqdm(unit="8", unit_scale=True, unit_divisor=1000, total=int(file_size)+5)
+
+        
+            
         while not done:
-            data += self.client.recv(1024)
+            received = self.client.recv(1040)
+            received = cipher.decrypt(received)
+            received = unpad(received, AES.block_size)
+
+            data += received
             if data[-5:] == b'<END>':
+                file_bytes = data[:-5]
                 done = True
             else:
                 file_bytes = data
-            progress.update(1024)
-            time.sleep(0.08)
+            progress.update(len(received))
+            time.sleep(0.15)
+        
   
         file.write(file_bytes)
         file.close()
 
+    def decryptCBC(self, iv, encrypted):
+        cipher = AES.new(self.session_key, AES.MODE_CBC, iv=iv)
+        plaintext = cipher.decrypt(encrypted)
+        plaintext = unpad(plaintext, AES.block_size).decode()
+
+        return plaintext
+
+    def decryptECB(self, text):
+        cipher = AES.new(self.session_key, AES.MODE_ECB)
+        plaintext = cipher.decrypt(text)
+        plaintext = unpad(plaintext, AES.block_size).decode()
+        
+        return plaintext
+
     def receive_text_from_client(self):
         return self.client.recv(1024).decode()
-
     
+    def receive_encrypted_from_client(self):
+        return self.client.recv(1024)
+
     async def receive_key(self):
-        return self.client.recv(1024).decode()
+        return self.client.recv(1024)
     
